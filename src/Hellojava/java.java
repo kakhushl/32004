a8129d1819d5758384a361b8f67e58802e031716
Maninder Dhaliwal <kakhushl@sheridancollege.ca>
2023-05-25 09:21:53 -0400

    Hello guys hru

---
+++ src/Hellojava/java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Hellojava;
+
+/**
+ *
+ * @author Khushleen Kaur 
+ */
+public class java {
+    public static void main(String[] args) {
+        System.out.println("Hello guys!! how are you");
+    }
+}
